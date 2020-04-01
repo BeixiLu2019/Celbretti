1a315d9e4d44f1749471de004aa890a70f3228f1
Beixi Lu <beixi_lu@yahoo.com>
2020-04-01 21:07:46 +0200

    -bakery

--- app/models/bakery.rb
+++
@@ -1,7 +0,0 @@
-class Bakery < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :delivery_cost, presence: true
-  validates :description, presence: true
-  # validates :ratings, inclusion: { in: [1, 2, 3, 4, 5] }
-end
